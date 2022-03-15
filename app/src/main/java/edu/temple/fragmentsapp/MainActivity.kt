1383d7952601cee9bb1cfa5fecadbc59ffb6a1bb
Karl Morris <karl.morris@temple.edu>
2022-03-15 12:22:31 -0400

    Using factory method for startup data

--- app/src/main/java/edu/temple/fragmentsapp/MainActivity.kt
+++ app/src/main/java/edu/temple/fragmentsapp/MainActivity.kt
@@ -2,15 +2,26 @@ package edu.temple.fragmentsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val fragment1 = RandomImageFragment.newInstance(R.drawable.cat1)
+        val fragment2 = RandomImageFragment.newInstance(R.drawable.cat2)
+
         supportFragmentManager.beginTransaction()
-            .add(R.id.container1, RandomImageFragment())
-            .add(R.id.container2, RandomImageFragment())
+            .add(R.id.container1, fragment1)
+            .add(R.id.container2, fragment2)
             .commit()
+
+        findViewById<Button>(R.id.changeButton).setOnClickListener {
+            fragment1.changeImage()
+        }
+
+        //fragment1.changeImage(R.drawable.cat1)
+        //fragment2.changeImage(R.drawable.cat2)
     }
 }
--- app/src/main/java/edu/temple/fragmentsapp/RandomImageFragment.kt
+++ app/src/main/java/edu/temple/fragmentsapp/RandomImageFragment.kt
@@ -10,24 +10,61 @@ import java.util.*
 
 class RandomImageFragment : Fragment() {
 
+    private var imageView : ImageView? = null
+    private var imageResource : Int? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.run{
+            imageResource = getInt("imageResource")
+        }
+    }
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         val layout = inflater.inflate(R.layout.fragment_random_image, container, false)
 
-        val imageView = layout.findViewById<ImageView>(R.id.imageView)
+        imageView = layout.findViewById<ImageView>(R.id.imageView)
 
-        imageView.setOnClickListener {
-
-            imageView.setImageResource(
-                if (Random().nextInt(2) > 0 ) R.drawable.cat1 else R.drawable.cat2
-            )
-
+        imageView!!.setOnClickListener {
+            changeImage()
         }
-
 
 
         return layout
 
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        imageResource?.run{
+            changeImage(this)
+        }
+    }
+
+
+    fun changeImage() {
+        imageView!!.setImageResource(
+            if (Random().nextInt(2) > 0 ) R.drawable.cat1 else R.drawable.cat2
+        )
+    }
+
+    fun changeImage(resourceId: Int) {
+        imageView!!.setImageResource(resourceId)
+    }
+
+
+    companion object {
+        fun newInstance(resourceId: Int) : RandomImageFragment {
+            val fragment = RandomImageFragment()
+            val bundle = Bundle()
+            bundle.putInt("imageResource", resourceId)
+            fragment.arguments = bundle
+            return fragment
+        }
     }
 
 }
